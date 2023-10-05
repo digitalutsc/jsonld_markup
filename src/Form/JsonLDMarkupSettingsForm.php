df2a3428360fd6ee798fab8ee73a05cd56987967
wilso929 <wilso929@LIB-C02D38MSML85.local>
2023-10-05 14:03:31 -0400

    more formatting

--- src/Form/JsonLDMarkupSettingsForm.php
+++ src/Form/JsonLDMarkupSettingsForm.php
@@ -12,44 +12,53 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class JsonLDMarkupSettingsForm extends FormBase {
 
-    const JSONLD_MARKUP_SETTINGS_PAGE = 'jsonld_markup_settings_page:values';
+  const JSONLD_MARKUP_SETTINGS_PAGE = 'jsonld_markup_settings_page:values';
 
-    public function getFormId()
-    {
-        return 'jsonld_markup_settings_page';
-    }
+  /**
+  * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'jsonld_markup_settings_page';
+  }
 
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-        $values = \Drupal::state()->get(self::JSONLD_MARKUP_SETTINGS_PAGE);
-        $form = [];
+  /**
+  * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $values = \Drupal::state()->get(self::JSONLD_MARKUP_SETTINGS_PAGE);
+    $form = [];
 
-        $form['schema_field'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t("Schema Field"),
-            '#description' => $this->t("The field the the schema type will be pulled from"),
-            '#required' => TRUE,
-            '#default_value' => $values['schema_field'],
+    $form['schema_field'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t("Schema Field"),
+        '#description' => $this->t("The field the the schema type will be pulled from"),
+        '#required' => TRUE,
+        '#default_value' => $values['schema_field'],
 
-        ];
+    ];
 
-        $form['actions']['#type'] = 'actions';
-        $form['actions']['submit'] = [
-            '#type' => 'submit',
-            '#value' => $this->t('Save'),
-            '#button_type' => 'primary'
-        ];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Save'),
+        '#button_type' => 'primary'
+    ];
 
-        return $form;
-    }
+    return $form;
+  }
 
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        $submitted_values = $form_state->cleanValues()->getValues();
+  /**
+  * {@inheritdoc}
+  */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $submitted_values = $form_state->cleanValues()->getValues();
 
-        \Drupal::state()->set(self::JSONLD_MARKUP_SETTINGS_PAGE, $submitted_values);
-        $messenger = \Drupal::service('messenger');
-        $messenger->addMessage($this->t("Configuration Saved"));
-    }
+    \Drupal::state()->set(self::JSONLD_MARKUP_SETTINGS_PAGE, $submitted_values);
+    $messenger = \Drupal::service('messenger');
+    $messenger->addMessage($this->t("Configuration Saved"));
+  }
 
 }

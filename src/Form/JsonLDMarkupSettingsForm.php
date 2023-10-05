f6332f4429b10157d41f39b52c65c75410d41b66
wilso929 <wilso929@LIB-C02D38MSML85.local>
2023-10-05 14:33:07 -0400

    more formatting

--- src/Form/JsonLDMarkupSettingsForm.php
+++ src/Form/JsonLDMarkupSettingsForm.php
@@ -15,45 +15,42 @@ class JsonLDMarkupSettingsForm extends FormBase {
   const JSONLD_MARKUP_SETTINGS_PAGE = 'jsonld_markup_settings_page:values';
 
   /**
-  * {@inheritdoc}
+   * {@inheritdoc}
    */
-  public function getFormId()
-  {
+  public function getFormId() {
     return 'jsonld_markup_settings_page';
   }
 
   /**
-  * {@inheritdoc}
+   * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
+  public function buildForm(array $form, FormStateInterface $form_state) {
     $values = \Drupal::state()->get(self::JSONLD_MARKUP_SETTINGS_PAGE);
     $form = [];
 
     $form['schema_field'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t("Schema Field"),
-        '#description' => $this->t("The field the the schema type will be pulled from"),
-        '#required' => TRUE,
-        '#default_value' => $values['schema_field'],
+      '#type' => 'textfield',
+      '#title' => $this->t("Schema Field"),
+      '#description' => $this->t("The field the the schema type will be pulled from"),
+      '#required' => TRUE,
+      '#default_value' => $values['schema_field'],
 
     ];
 
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('Save'),
-        '#button_type' => 'primary'
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
     ];
 
     return $form;
   }
 
   /**
-  * {@inheritdoc}
-  */
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
     $submitted_values = $form_state->cleanValues()->getValues();
 
     \Drupal::state()->set(self::JSONLD_MARKUP_SETTINGS_PAGE, $submitted_values);
